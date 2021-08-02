18673ed141fb8d99774a8ed251e582bde94f8182
Consecratio <jcasique@gmail.com>
2021-08-02 14:24:33 -0700

    added navbar and jumbotron

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,14 @@
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron" style={{ height: "100vh", marginTop: "25vh" }}>
+            <h1 className="display-6">FULLSTACK ENGINEER</h1>
+            <h1 className="display-6">READY TO MAKE AN IMPACT</h1>
+            <p className="lead">Specializing in Full-Stack development utilizing technologies like: MongoDB, Express.js, React.js, Node.js</p>
+            <hr className="my-4" />
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <a className="btn btn-primary btn-lg" href="#home">Learn more</a>
+        </div>
+    )
+}
+
+export default Jumbotron
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import { Navbar, Nav, Container } from "react-bootstrap"
+
+const Navigation = () => {
+    return (
+        <Navbar collapseOnSelect expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand href="#" style={{ fontWeight: "bold" }}>Jorge.<span style={{ color: "#7F5AF0" }}>Casique</span></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="col-3 me-5"></Nav>
+                    <Nav
+                    className="mr-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                    >
+                        <Nav.Link href="#action1">HOME</Nav.Link>
+                        <Nav.Link href="#action2">PROJECTS</Nav.Link>
+                        <Nav.Link href="#action3">ABOUT ME</Nav.Link>
+                        <Nav.Link href="#action4">CONTACT</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation
