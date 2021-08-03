24b2c6d40a740d372774e8a1cf4231b61b7f8159
Consecratio <jcasique@gmail.com>
2021-08-03 15:40:56 -0700

    fixed issues with navbar in mobile version

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,16 +2,14 @@ import { Navbar, Nav, Container } from "react-bootstrap"
 
 const Navigation = () => {
     return (
-        <Navbar collapseOnSelect expand="md" fixed="top">
+        <Navbar collapseOnSelect expand="lg" fixed="top" style={{ backgroundColor: "white" }}>
             <Container>
                 <Navbar.Brand href="#" style={{ fontWeight: "bold" }}>Jorge.<span style={{ color: "#7F5AF0" }}>Casique</span></Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="col-lg-2 me-5"></Nav>
                     <Nav
                     className="mr-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
-                    navbarScroll
                     >
                         <Nav.Link href="#action1">HOME</Nav.Link>
                         <Nav.Link href="#action2">TECHNOLOGIES</Nav.Link>
