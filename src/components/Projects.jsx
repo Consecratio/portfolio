dd98e73de1216587dbc1e5171c9fbd823d08f236
Consecratio <jcasique@gmail.com>
2021-08-03 15:23:33 -0700

    added images to projects, need to work on descriptions and links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,18 @@
 import { Row, Col } from 'react-bootstrap'
+import viberant from '../img/viberant.png'
 
 const Projects = () => {
     return (
         <div className="container">
-            <h2>Projects</h2>
-            <Row lg={1}>
+            <Row className="text-center" style={{ color: "#94A1B2" }}>
+                <h3 style={{ margin: "50px 0" }}>PROJECTS</h3>
+            </Row>
+            <Row lg={2}>
                 <Col>
-                    <img src="" />
+                    <img src={viberant} alt="Viberant" style={{ height: "50vh", margin: "0 auto", display: "block" }} />
+                </Col>
+                <Col>
+                    <img src={viberant} alt="Viberant" style={{ height: "50vh", margin: "0 auto", display: "block" }} />
                 </Col>
             </Row>
         </div>
