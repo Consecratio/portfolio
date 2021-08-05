628f243feb3c179e3c1507b6cdc5e55dfec47da1
Consecratio <jcasique@gmail.com>
2021-08-05 10:03:57 -0700

    added animations to jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -5,17 +5,17 @@ import './Jumbotron.css'
 const Jumbotron = () => {
     return (
         <div id="home" className="jumbotron" style={{ minHeight: "75vh", marginTop: "25vh" }}>
-            <Row xs={1} md={1} lg={2}>
-                <Col className="mb-5">
-                    <h1 className="display-6" style={{ fontWeight: "900" }}>FULLSTACK ENGINEER</h1>
-                    <h1 className="display-6" style={{ fontWeight: "900" }}>READY TO MAKE AN IMPACT</h1>
-                    <p className="lead">Specializing in Full-Stack development utilizing technologies like: MongoDB, Express.js, React.js, Node.js</p>
+            <Row xs={1} md={1} lg={2} >
+                <Col className="mb-5 slide" >
+                    <h1 className="display-6" style={{ fontWeight: "900" }} >FULLSTACK ENGINEER</h1>
+                    <h1 className="display-6" style={{ fontWeight: "900" }} >READY TO MAKE AN IMPACT</h1>
+                    <p className="lead" >Specializing in Full-Stack development utilizing technologies like: MongoDB, Express.js, React.js, Node.js</p>
                     <hr className="my-4" />
                     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <a className="btn btn-primary btn-lg" href="#home">Learn more</a>
+                    <a className="btn btn-primary btn-lg" href="#home" >Learn more</a>
                 </Col>
                 <Col lg={{ span: 3, offset: 2 }} className="mb-5" >
-                    <img src={myPhoto} alt="Jorge Casique" style={{ height: "60vh" }} className="mx-auto d-block" />
+                    <img src={myPhoto} alt="Jorge Casique" style={{ height: "60vh" }} className="mx-auto d-block my-photo" />
                 </Col>
                 <div id="jumbo-div" ></div>
             </Row>
