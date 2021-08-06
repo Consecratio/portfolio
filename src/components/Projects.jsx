6de112299910190461daf5a393b50899bff26548
Consecratio <jcasique@gmail.com>
2021-08-06 10:39:12 -0700

    added rel for opening _blank pages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,40 +10,40 @@ const Projects = () => {
             <Row className="text-center" style={{ color: "#94A1B2" }}>
                 <h3 style={{ margin: "50px 0" }}>PROJECTS</h3>
             </Row>
-            <Row lg={2} className="align-items-center" >
+            <Row lg={2} className="align-items-center" style={{ height: "100vh" }} >
                 <Col>
                     <img src={viberant} alt="Viberant" style={{ height: "100%", maxWidth: "100%", margin: "0 auto", display: "block" }} />
                 </Col>
                 <Col className="mx-auto">
                     <h3 className="project-name">Viberant</h3>
                     <p className="tech-stack">MongoDB + Express.js + React.js + Node.js + Mongoose + Axios + REST API + Bootstrap</p>
-                    <a href="https://admiring-spence-c2dab0.netlify.app/" target="_blank" className="project-links">Live Site</a>
+                    <a href="https://admiring-spence-c2dab0.netlify.app/" target="_blank" rel="noopener noreferrer" className="project-links">Live Site</a>
                     <br />
-                    <a href="https://github.com/Consecratio/mern-auth-client" target="_blank" className="project-links">Github</a>
+                    <a href="https://github.com/Consecratio/mern-auth-client" target="_blank" rel="noopener noreferrer" className="project-links">Github</a>
                 </Col>
             </Row>
-            <Row lg={2} className="align-items-center flex-row-reverse" >
+            <Row lg={2} className="align-items-center flex-row-reverse" style={{ height: "100vh" }} >
                 <Col>
                     <img src={spellbook} alt="Spellbook" style={{ maxHeight: "50vh", maxWidth: "100%", margin: "0 auto", display: "block" }} />
                 </Col>
                 <Col className="mx-auto">
                     <h3 className="project-name">SpellBook</h3>
                     <p className="tech-stack">PostgreSQL + Express.js + EJS + Node.js + Sequelize + Axios + REST API + Bootstrap</p>
-                    <a href="https://spellbook-project.herokuapp.com/" target="_blank" className="project-links">Live Site</a>
+                    <a href="https://spellbook-project.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="project-links">Live Site</a>
                     <br />
-                    <a href="https://github.com/Consecratio/project-2-spellbook" target="_blank" className="project-links">Github</a>
+                    <a href="https://github.com/Consecratio/project-2-spellbook" target="_blank" rel="noopener noreferrer" className="project-links">Github</a>
                 </Col>
             </Row>
-            <Row lg={2} className="align-items-center" >
+            <Row lg={2} className="align-items-center" style={{ height: "100vh" }} >
                 <Col>
                     <img src={chatApp} alt="Chat App" style={{ height: "50vh", margin: "0 auto", display: "block" }} />
                 </Col>
                 <Col className="mx-auto">
                     <h3 className="project-name">Chat App</h3>
                     <p className="tech-stack">Express.js + React.js + Node.js + Socket.io + Bootstrap</p>
-                    <a href="https://practical-visvesvaraya-c80ca0.netlify.app/" target="_blank" className="project-links">Live Site</a>
+                    <a href="https://practical-visvesvaraya-c80ca0.netlify.app/" target="_blank" rel="noopener noreferrer" className="project-links">Live Site</a>
                     <br />
-                    <a href="https://github.com/Consecratio/chat-app-client" target="_blank" className="project-links">Github</a>
+                    <a href="https://github.com/Consecratio/chat-app-client" target="_blank" rel="noopener noreferrer" className="project-links">Github</a>
                 </Col>
             </Row>
         </div>
