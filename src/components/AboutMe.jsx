322fda197c31805b99ad05d1eac5bd695d962928
Consecratio <jcasique@gmail.com>
2021-08-06 14:17:42 -0700

    altered About Me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -13,7 +13,7 @@ const AboutMe = () => {
                 <Row>
                     <Col>
                         <p className="about-me-text text-center">
-                            I am a Software Engineer with 5 years experience in design, management, and working in a team environment both in person, and remote. I enjoy learning new technologies, and continue to push myself and my skills to new heights. I specialize in full-stack JavaScript (PostgreSQL, Express.js, React.js, Node.js) and RESTful APIs.
+                            I am a Software Engineer with 5 years experience in design, management, and working in a team environment both in person, and remote. I enjoy learning new technologies, and continue to push myself and my skills to new heights. I specialize in full-stack MERN/PERN (MongoDB, PostgreSQL, Express.js, React.js, Node.js) and RESTful APIs.
                         </p>
                     </Col>
                 </Row>
